01a8f5268b067830c3de4aa9e544e71b1eb46164
DJA <DJA@DJA-PC>
2017-09-21 19:06:08 +0900

    2017-09-21

--- iot1_sql/iot1_sql/src/main/java/com/iot1/sql/common/test/Cal.java
+++ iot1_sql/iot1_sql/src/main/java/com/iot1/sql/common/test/Cal.java
@@ -5,6 +5,15 @@ public class Cal {
 	   public int plus(int a, int b){
 	      return a + b;
 	   }
+	   public int minus(int a,int b){
+		   return a - b;
+	   }
+	   public int division(int a,int b){
+		   return a / b;
+	   }
+	   public int multiple(int a,int b){
+		   return a * b;
+	   }
 	}
   
 
--- iot1_sql/iot1_sql/src/main/java/com/iot1/sql/common/test/CalTest.java
+++ iot1_sql/iot1_sql/src/main/java/com/iot1/sql/common/test/CalTest.java
@@ -3,15 +3,47 @@ package com.iot1.sql.common.test;
 
 import static org.junit.Assert.assertEquals;
 
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalTest {
 
-   @Test
-   public void testPlus(){
-      Cal c = new Cal();
-      int result = c.plus(20, 20);
-      assertEquals("20+20=40", 40, result);
-   }
-}
+	   Cal tdd;
+
+	   @Before
+	   public void setUpBefore() throws Exception {
+	      System.out.println(tdd);
+	      tdd = new Cal();
+	      System.out.println(tdd);
+	   }
+
+	   @Test
+	   public void testPlus() {
+	      int result = tdd.plus(10, 20);
+	      assertEquals("10+20=30",30, result);
+	   }
+
+	   @Test
+	   public void testMinus() {
+	      int result = tdd.minus(20, 10);
+	      assertEquals("20-10=10", 10, result);
+	   }
+
+	   @Test
+	   public void testDivision() {
+	      int result = tdd.division(20,10);
+	      assertEquals("20/10=2", 2 , result);
+	   }
+
+	   @Test
+	   public void testMultiple() {
+	      int result = tdd.multiple(10, 20);
+	      assertEquals("10*20=200", 200, result);
+	   }
+	   @After
+	   public void setUpAfter() throws Exception {
+	      System.out.println(tdd);
+	      tdd = null;
+	   }
+	}
